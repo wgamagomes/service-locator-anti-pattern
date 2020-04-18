5d79e039e43e47216b9737d789c62833469bf0af
Wellington G Gomes <wgamagomes@gmail.com>
2020-04-18 20:25:18 -0300

    [ADD] Added GenericRepository class

---
+++ ServiceLocatorAntiPattern/ServiceLocatorAntiPattern/GenericRepository.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections.Generic;
+
+namespace ServiceLocatorAntiPattern
+{
+    public class  GenericRepository<TEntity>: IGenericRepository<TEntity>
+
+        where TEntity: class, new()
+    {
+       public IEnumerable<TEntity> GetAll()
+        {
+            //Some db logic to fetch Entities information 
+            return (new List<TEntity>());
+        }
+    }
+}
