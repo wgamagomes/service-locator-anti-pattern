c91794e8b8f0c7eaeac4b75396c6c14f292cd02b
Wellington G Gomes <wgamagomes@gmail.com>
2020-05-09 16:22:12 -0300

    [ALT] Alter IGenericRepository namespace

---
+++ ServiceLocatorAntiPattern/ServiceLocatorAntiPattern/Interfaces/IGenericRepository.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections.Generic;
+
+namespace ServiceLocatorAntiPattern.Interfaces
+{
+    public interface  IGenericRepository<TEntity> where TEntity: class, new()
+    {
+        IEnumerable<TEntity> GetAll();
+    }
+}
