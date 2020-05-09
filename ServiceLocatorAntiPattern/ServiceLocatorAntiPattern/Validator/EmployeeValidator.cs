e5caf7860710605ba5f3bc6cc1ee7628a1742a81
Wellington G Gomes <wgamagomes@gmail.com>
2020-05-09 17:47:43 -0300

    [ADD] Added EmployeeValidator class

---
+++ ServiceLocatorAntiPattern/ServiceLocatorAntiPattern/Validator/EmployeeValidator.cs
@@ -0,0 +1,13 @@
+﻿using ServiceLocatorAntiPattern.Interfaces;
+
+namespace ServiceLocatorAntiPattern.Validator
+{
+    public class EmployeeValidator : IEmployeeValidator
+    {
+        public bool Validate(Employee employee)
+        {
+            return !string.IsNullOrEmpty(employee.FirstName)
+                && !string.IsNullOrEmpty(employee.LastName);
+        }
+    }
+}
