d5be31de384d347135c91db389123c7da34850ed
Wellington G Gomes <wgamagomes@gmail.com>
2020-05-09 21:06:35 -0300

    [ALT] Alter program class

---
+++ ServiceLocatorAntiPattern/ServiceLocatorAntiPattern/Program.cs
@@ -0,0 +1,22 @@
+﻿using ServiceLocatorAntiPattern.Entities;
+using ServiceLocatorAntiPattern.Interfaces;
+using ServiceLocatorAntiPattern.Repositories;
+using ServiceLocatorAntiPattern.Services;
+using ServiceLocatorAntiPattern.Validator;
+using System;
+using System.Threading.Tasks;
+
+namespace ServiceLocatorAntiPattern
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            Bootstrapper.Register();
+
+            var employeeService = new EmployeeService();
+
+            await employeeService.HandleAsync(new Employee { FirstName = "Ton", LastName = "Gomes" });
+        }
+    }
+}
