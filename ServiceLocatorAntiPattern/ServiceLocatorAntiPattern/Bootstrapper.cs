0c42bcb5eeae52f018d12da527c8c49fa8386386
Wellington G Gomes <wgamagomes@gmail.com>
2020-05-09 21:59:21 -0300

    [ADD] Added Bootstrapper class

---
+++ ServiceLocatorAntiPattern/ServiceLocatorAntiPattern/Bootstrapper.cs
@@ -0,0 +1,20 @@
+﻿using ServiceLocatorAntiPattern.Interfaces;
+using ServiceLocatorAntiPattern.Locator;
+using ServiceLocatorAntiPattern.Repositories;
+using ServiceLocatorAntiPattern.Validator;
+
+namespace ServiceLocatorAntiPattern
+{
+    public class Bootstrapper
+    {
+        public static void Register()
+        {
+            ServiceLocator.Register<IEmployeeValidator, EmployeeValidator>(() => new EmployeeValidator());
+            ServiceLocator.Register<IEmployeeRepository, EmployeeRepository>(() => new EmployeeRepository());
+            ServiceLocator.Register<IUnitOfWork, UnitOfWork>(() => new UnitOfWork());
+
+        }
+    }
+}
+
+
