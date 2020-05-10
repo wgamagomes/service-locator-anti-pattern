e5e3533fedaa0f30f6f5ab926b590f7ef546bda4
Wellington G Gomes <wgamagomes@gmail.com>
2020-05-10 10:56:40 -0300

    [ALT] Added IUnitOfWork interface

---
+++ ServiceLocatorAntiPattern/ServiceLocatorAntiPattern/Interfaces/IUnitOfWork.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ServiceLocatorAntiPattern.Interfaces
+{
+    public interface IUnitOfWork
+    {
+        Task<bool> CommitAsync();
+    }
+}
