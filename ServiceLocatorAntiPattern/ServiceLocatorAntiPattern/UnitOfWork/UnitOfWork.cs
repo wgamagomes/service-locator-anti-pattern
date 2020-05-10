93054e270a4e3f15dad1f123dac57cc7b0a9d4d1
Wellington G Gomes <wgamagomes@gmail.com>
2020-05-10 11:01:53 -0300

    [ADD] Added UnitOfWork class

---
+++ ServiceLocatorAntiPattern/ServiceLocatorAntiPattern/UnitOfWork/UnitOfWork.cs
@@ -0,0 +1,13 @@
+﻿using ServiceLocatorAntiPattern.Interfaces;
+using System.Threading.Tasks;
+
+namespace ServiceLocatorAntiPattern
+{
+    public class UnitOfWork : IUnitOfWork
+    {
+        public Task<bool> CommitAsync()
+        {
+            return Task.Run(() => true);
+        }
+    }
+}
