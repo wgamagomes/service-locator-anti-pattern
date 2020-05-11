ffdf6df9c38c1b1e15df1ea11d6e943936f55eeb
Wellington G Gomes <wgamagomes@gmail.com>
2020-05-11 08:51:26 -0300

    [ALT] Alter dir

---
+++ ServiceLocatorAntiPattern/ServiceLocatorAntiPattern/Repositories/EmployeeRepository.cs
@@ -0,0 +1,10 @@
+﻿using ServiceLocatorAntiPattern.Entities;
+using ServiceLocatorAntiPattern.Interfaces;
+
+namespace ServiceLocatorAntiPattern.Repositories
+{
+    public class EmployeeRepository : GenericRepository<Employee> , IEmployeeRepository
+    {
+       
+    }
+}
