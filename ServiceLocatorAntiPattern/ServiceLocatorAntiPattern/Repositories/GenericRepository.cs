8b18d8bcce2c0af659ac3147a3fb656e6c3acd9a
Wellington G Gomes <wgamagomes@gmail.com>
2020-05-11 08:50:48 -0300

    [ALT] GenericRepository class

---
+++ ServiceLocatorAntiPattern/ServiceLocatorAntiPattern/Repositories/GenericRepository.cs
@@ -0,0 +1,29 @@
+﻿using ServiceLocatorAntiPattern.Interfaces;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace ServiceLocatorAntiPattern.Repositories
+{
+    public class  GenericRepository<TEntity>: IGenericRepository<TEntity>
+
+        where TEntity: class, new()
+    {
+        private List<TEntity> _entities;
+        public GenericRepository()
+        {
+            _entities = new List<TEntity>();
+        }
+       public IEnumerable<TEntity> GetAll()
+        {
+            //Some db logic to fetch Entities information 
+            return _entities;
+        }
+
+        public async Task InsertAsync(TEntity entity)
+        {
+           await  Task.Run(() => {
+                _entities.Add(entity);
+            });           
+        }
+    }
+}
