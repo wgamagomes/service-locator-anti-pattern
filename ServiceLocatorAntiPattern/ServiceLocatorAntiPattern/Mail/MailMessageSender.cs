ac2ba667c0f8f88bf5079611af24b3b409ab6f88
Wellington G Gomes <wgamagomes@gmail.com>
2020-05-12 08:25:14 -0300

    [ADD] Added MailMessageSender class

---
+++ ServiceLocatorAntiPattern/ServiceLocatorAntiPattern/Mail/MailMessageSender.cs
@@ -0,0 +1,13 @@
+﻿using ServiceLocatorAntiPattern.Interfaces;
+using System.Threading.Tasks;
+
+namespace ServiceLocatorAntiPattern.Mail
+{
+    public class MailMessageSender : IMailMessageSender
+    {
+        public Task<bool> Send<TEntity>(TEntity entity)
+        {
+            return Task.Run(() => true);
+        }
+    }
+}
